1d972e2e83af9f94b3f31cfff3f1938bbba6cd1e
Shubha Sarkar <140386678+ShubhaSarkar12@users.noreply.github.com>
2024-02-15 12:08:23 +0530

    Add files via upload

---
+++ Leaf disease prediction using Machine Learning/app/app.py
@@ -0,0 +1,163 @@
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+from skimage import io
+from tensorflow.keras.preprocessing import image
+
+
+    
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template,session
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+
+# Define a flask app
+app = Flask(__name__)
+
+app.secret_key = 'xyzsdfg'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user-system'
+  
+mysql = MySQL(app)
+
+
+
+
+# Model saved with Keras model.save()
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+
+model =tf.keras.models.load_model('PlantDNet.h5',compile=False)
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+@app.route('/', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return render_template('index.html', mesage = mesage)
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage = mesage)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+  
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password, ))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
+
+
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    show_img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = np.array(x, 'float32')
+    x /= 255
+    preds = model.predict(x)
+    return preds
+
+
+
+
+
+
+@app.route('/index', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        print(preds[0])
+        
+
+
+        # f = f.reshape([64, 64]);
+        disease_class = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight',
+                         'Potato___Late_blight', 'Potato___Alternaria', 'Tomato_Bacterial_spot', 'Tomato_Early_blight',
+                         'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
+                         'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
+                         'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy','Pepper__bell___Bacterial_spot']
+        predicted_label = disease_class[np.argmax(preds)]
+        return render_template('predict.html', prediction=predicted_label)
+
+        a = preds[0]
+        ind=np.argmax(a)
+        print('Prediction:', disease_class[ind])
+        result=disease_class[ind]
+        print(result)
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+    app.run()
